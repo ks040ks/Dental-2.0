e9cfd21f5c6d2205cdf19eefe73fe154bdee6625
SUKUNA <156893776+SUKUNA-001@users.noreply.github.com>
2024-08-09 15:18:43 +0530

    Create doctor.js

---
+++ frontend/src/assets/data/doctor.js
@@ -0,0 +1,27 @@
+
+
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Vishal Sharma",
+    Role: "Dentist",
+    avgRating: 4.8,
+    totalRating: 272,
+   
+    totalPatients: 1500,
+    clinic: "Jagdish Dental care, Badhroya.",
+
+  },  
+  {
+    id: "02",
+    name: "Dr. hfnig Sharma",
+    Role: "Dentist",
+    avgRating: 4.8,
+    totalRating: 272,
+    
+    totalPatients: 1500,
+    clinic: "Jagdish Dental care, Badhroya.",
+
+  },  
+];
