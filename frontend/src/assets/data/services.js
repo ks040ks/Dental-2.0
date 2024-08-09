7acb48d4592d34d30bb1ffc78921ae02d1ab9587
SUKUNA <156893776+SUKUNA-001@users.noreply.github.com>
2024-08-09 15:18:46 +0530

    Create services.js

---
+++ frontend/src/assets/data/services.js
@@ -0,0 +1,41 @@
+
+
+export const services = [
+    {
+      name: "Smile Makeover",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(254, 182, 13, .2)",
+     
+      textColor: "#FEB60D",
+    },
+    {
+      name: "Dental X-Ray",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(151, 113, 255, .2)",
+      textColor: "#9771FF",
+    },
+    {
+      name: "R C T",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(1, 181, 197, .2)",
+      textColor: "#01B5C5",
+    },
+    {
+      name: "Impalant Procedure",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(1, 181, 197, .2)",
+      textColor: "#01B5C5",
+    },
+    {
+      name: "OrthoDontic Solution",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(254, 182, 13, .2)",
+      textColor: "#FEB60D",
+    },
+    {
+      name: "Pediatric Dentistry",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(151, 113, 255, .2)",
+      textColor: "#9771FF",
+    },
+  ];
