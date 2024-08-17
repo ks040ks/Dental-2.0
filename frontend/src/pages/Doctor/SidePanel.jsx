76aa1ff93ff0756e392b7b9272b01547ef86f547
SUKUNA <156893776+SUKUNA-001@users.noreply.github.com>
2024-08-17 20:52:06 +0530

    Update SidePanel.jsx

--- frontend/src/pages/Doctor/SidePanel.jsx
+++ frontend/src/pages/Doctor/SidePanel.jsx
@@ -1,43 +1,39 @@
 import React from 'react'
+import convertTime from '../../utils/convertTime'
 
-const SidePanel = () => {
+const SidePanel = ({doctorId, FeePrice, timeSlots}) => {
   return (
     <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
      <div className='flex items-center justify-between'>
       <p className='mt-0 text-[18px] leading-[30px] font-semibold text-textColor '>
         Appointment Fee</p>
         <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor
-        font-bold'>29-/ RS</span>
+        font-bold'>{FeePrice} 31-/</span>
      </div>
 
      <div className='mt-[30px]'>
       <p className='text-[18px] leading-[30px] font-semibold text-headingColor mt-0'>
         Available Time Slots: At Jagdish Dental Clinic, Badhroya.
       </p>
-      <ul className='mt-3'>
-        <li className='flex items-center justify-between mb-2'>
+      
+       <ul className='mt-3'>
+      {timeSlots?.map((item,index)=> (
+             <li key={index} className='flex items-center justify-between mb-2'>
           <p className='text-[15px] leading-6 text-textColor font-semibold'>
-            Sunday
+            {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
           </p>
           <p className='text-[15px] leading-6 text-textColor font-semibold'>
-            9:00 AM - 2:00 PM
-          </p>
-        </li>
-        <li className='flex items-center justify-between mb-2'>
-          <p className='text-[15px] leading-6 text-textColor font-semibold'>
-            Mon - Sat 
-          </p>
-          <p className='text-[15px] leading-6 text-textColor font-semibold'>
-            5:00 PM - 8:30 PM
+            {item.startingTime} - {item.endingTime}
           </p>
         </li>
+        ))}
         
       </ul>
      </div>
 
      <div className='mt-[30px]'>
       <p className='text-[18px] leading-[30px] font-semibold text-headingColor mt-0'>
-        Available Time Slots: At Jagdish Dental Clinic, Badhroya.
+        Available Time Slots: At Shella Dental Clinic, Sundar Chack.
       </p>
       <ul className='mt-3'>
         <li className='flex items-center justify-between mb-2'>
