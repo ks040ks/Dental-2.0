2ffff76535f8696bdc0154da6e318edc0e596ce7
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-09-19 22:27:47 +0530

    Update doctor.js

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -16,11 +16,11 @@ const router = express.Router()
 router.use("/:doctorId/reviews", reviewRouter)
 
 
-router.get('/:id', getSingleDoctor)
+router.get("/:id", getSingleDoctor)
 router.get("/", getAllDoctor)
 router.put("/:id", authenticate,restrict(['doctor']), updateDoctor)
 router.delete("/:id",authenticate,restrict(['doctor']), deleteDoctor)
-router.get('/profile/me', authenticate,restrict(['doctor']), getDoctorProfile)
+router.get("/profile/me", authenticate,restrict(['doctor']), getDoctorProfile)
 
 // router.put("/:id", authenticate,restrict(['doctor']), updateDoctor)
 // router.delete("/:id", authenticate,restrict(['doctor']),deleteDoctor)
