5355ce7e6071e33959c019c422dcf54bf797088f
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-09-19 22:28:48 +0530

    Update Doctor.jsx

--- frontend/src/component/Doctor/Doctor.jsx
+++ frontend/src/component/Doctor/Doctor.jsx
@@ -1,19 +1,22 @@
-// import useFetchData from "../../hooks/useFetchData.jsx"
-// import { BASE_URL } from "../../config.js"
+import useFetchData from "../../hooks/useFetchData.jsx"
+import { BASE_URL } from "../../config.js"
 import DoctorCard from "./DoctorCard.jsx";
-// import Loader from "../../component/Loader/Loading.jsx"
-// import Error from "../../component/Error/Error.jsx"
-import {doctors} from "./../../assets/data/doctor.js"
+import Loader from "../../component/Loader/Loading.jsx"
+import Error from "../../component/Error/Error.jsx"
+
 const Doctor = () => {
 
-  // const {data: doctors, loading, error} = useFetchData(`${BASE_URL}/doctor`)
+   const {data: doctors, loading, error} = useFetchData(`${BASE_URL}/doctor`)
   return (
   <>
- <div className='grid grid-col-1 sm:grid-cols-2 
+  {loading && <Loader/>}
+  {error && <Error/>}
+
+ {!loading && !error && <div className='grid grid-col-1 sm:grid-cols-2 
       gap-5 lg:gap-[30px] mt-[30px] lg-mt-[55px]'>
-    {doctors.map((doctor) =>
-       { return <DoctorCard key={doctor.id} doctor={doctor} /> })}
-    </div>
+    {doctors.map(doctor =>(
+        <DoctorCard key={doctor._id} doctor={doctor} /> ))}
+    </div>}
     </>
   )
 }
