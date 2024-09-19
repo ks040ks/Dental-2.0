94d9ccc3459726d71ef73958c288a922d213bd38
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-09-19 22:28:28 +0530

    Update BokkingSchema.js

--- backend/models/BokkingSchema.js
+++ backend/models/BokkingSchema.js
@@ -1,44 +1,38 @@
 import mongoose from "mongoose";
 
-const DoctorSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  phone: { type: Number },
-  photo: { type: String },
-  ticketPrice: { type: Number },
-  role: {
-    type: String,
+const bookingSchema = new mongoose.Schema(
+  {
+    doctor: {
+      type: mongoose.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    feePrice: { type: String, required: true },
+    
+    status: {
+      type: String,
+      enum: ["pending", "approved", "cancelled"],
+      default: "pending",
+    },
+    isPaid: {
+      type: Boolean,
+      default: true,
+    },
   },
+  { timestamps: true }
 
-  // Fields for doctors only
-  specialization: { type: String },
-  qualifications: {
-    type: Array,
-  },
+);
 
-  experiences: {
-    type: Array,
-  },
+// bookingSchema.pre(/^find/, function(next){
+//   this.populate('user').populate({
+//     path:'doctor',
+//     select:"name"
+//   })
+// })
 
-  bio: { type: String, maxLength: 50 },
-  about: { type: String },
-  timeSlots: { type: Array },
-  reviews: [{ type: mongoose.Types.ObjectId, ref: "Review" }],
-  averageRating: {
-    type: Number,
-    default: 0,
-  },
-  totalRating: {
-    type: Number,
-    default: 0,
-  },
-  isApproved: {
-    type: String,
-    enum: ["pending", "approved", "cancelled"],
-    default: "pending",
-  },
-  appointments: [{ type: mongoose.Types.ObjectId, ref: "Appointment" }],
-});
-
-export default mongoose.model("Doctors", DoctorSchema);
+export default mongoose.model("Booking", bookingSchema);
