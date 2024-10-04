923e71a05cf6cf85f85ce81ddb330a9d60f44e51
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-10-04 10:57:49 +0530

    Update DoctorAbout.jsx

--- frontend/src/pages/Doctor/DoctorAbout.jsx
+++ frontend/src/pages/Doctor/DoctorAbout.jsx
@@ -7,11 +7,11 @@ const DoctorAbout = ({name,about, qualification,experiences}) => {
       <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
         About of 
         <span className='text-irisBlueColor'>
-            vishal sharma 
+            {name}
             </span>
       </h3>
       <p className='text-[18px] leading-[30px] font-[400] text-textColor mt-[18px]'>
-       i dv hdasyufdbyu fjfvthfdabggfxd hjfgtfdxjgf
+       {about}
       </p>
     </div>
     
@@ -21,40 +21,22 @@ const DoctorAbout = ({name,about, qualification,experiences}) => {
         </h3>
 
         <ul className='pt-4 md:p-5'>
-        <li 
-       className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]
-       `'>
-                 <div>
-                      <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
-                      {formatDate("12-12-12")} - {formatDate("12-12-12")}
-                      </span>
-
-                  <p className='text-[16px] leading-6 font-medium text-textColor'
-                  > BDS (Bachealor of Dental Surgery)
-                  </p>
-                  </div>
-                  <p className='text-[16px] leading-6 font-medium text-textColor'
-                  >Himachal Pradesh university 
-                  </p>
-                  </li>
-
-        {/* {qualification?.map((item,index)=>
+        {qualification?.map((item,index)=>
        <li key={index} 
-       className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]
-       `'>
-                 <div>
+       className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                 
                       <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
                       {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
                       </span>
 
-                  <p className='text-[16px] leading-6 font-medium text-textColor'
-                  > {item.degree}
+                  <p className='text-[16px] leading-6 font-medium text-textColor'>
+                    {item.degree}
                   </p>
-                  </div>
-                  <p className='text-[16px] leading-6 font-medium text-textColor'
-                  >{item.university}
+                  
+                  <p className='text-[16px] leading-6 font-medium text-textColor'>
+                    {item.university}
                   </p>
-                  </li> )} */}
+                  </li> )}
        
         </ul>
      
@@ -67,22 +49,20 @@ const DoctorAbout = ({name,about, qualification,experiences}) => {
       </h3>
 
       <ul className='grid sm:grid-cols-2  gap-[30px] pt-4 mb:p-5'>
-        {/* {experiences?.map((item,index)=> */}
-      <li  className='p-4 roounded bg-[#fff9ea]'>
+        {experiences?.map((item,index)=>
+      <li key={index}  className='p-4 roounded bg-[#fff9ea]'>
           <span className='text-yellowColor text-[15px] leading-6 font-semibold '>
-            {formatDate("12-12-12")} - {formatDate("12-12-12")}
+            {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
           </span>
           <p className='text-[16px] leading-6 font-medium text-textColor'>
-          sr. dental surgeon
+         {item.position}
           </p>
 
           <p className='text-[14px] leading-5 font-medium text-textColor'>
-          civil hospital
+          {item.hospital}
           </p>
         
-      </li>
-             
-    
+      </li>)}       
          </ul>
       
     </div>
