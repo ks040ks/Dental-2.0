c69482e64e27e69c891f28c2fd07e533982d56ed
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-10-04 10:58:00 +0530

    Update DoctorDetail.jsx

--- frontend/src/pages/Doctor/DoctorDetail.jsx
+++ frontend/src/pages/Doctor/DoctorDetail.jsx
@@ -3,26 +3,40 @@ import starIcon from "../../assets/images/Star.png"
 import DoctorAbout from './DoctorAbout'
 import Feedback from './Feedback'
 import SidePanel from './SidePanel'
-//import useFetchData from "../../hooks/useFetchData.jsx"
-// import { BASE_URL } from "../../config.js"
-// import Loader from "../../component/Loader/Loading.jsx"
-// import Error from "../../component/Error/Error.jsx"
-// import { useParams } from 'react-router-dom'
-import photo from "../../../src/assets/images/x.png"
+import useFetchData from "../../hooks/useFetchData.jsx"
+import { BASE_URL } from "../../config.js"
+import Loader from "../../component/Loader/Loading.jsx"
+import Error from "../../component/Error/Error.jsx"
+import { useParams } from 'react-router-dom'
+
 
 const DoctorDetail = () => {
 
-  // const [tab,setTab] = useState('about')
+  const [tab,setTab] = useState('about')
 
-  // const {id} = useParams()
+  const {id} = useParams()
 
-  // const {  loading,error} = useFetchData(`${BASE_URL}/doctor/${id}`);
+  const {data:doctor , loading,error} = useFetchData(`${BASE_URL}/doctor/${id}`);
 
-
+  const {
+    name,
+    qualification,
+    experiences,
+    timeSlots,
+    reviews,
+    bio,
+    about,
+    averageRating,
+    totalRating,
+    specialization,
+    feePrice,
+    photo
+  } = doctor;
 
   return (
    <section>
-    <div className='max-w-[1170px] px-5 mx-auto'>
+    {!loading && !error &&  
+    (<div className='max-w-[1170px] px-5 mx-auto'>
       <div className='grid md:grid-cols-3 gap-[50px]'>
 <div className='md:col-span-2'>
         <div className='flex items-center gap-5'>
@@ -33,22 +47,23 @@ const DoctorDetail = () => {
           <div>
             <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2
             lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold'>
-              hduybty u fchfncgcu bchgcfd fd
+              {specialization}
               </span>
               <h3 className='text-headingColor text-[22px] leading-9 mt-3 font-bold'
-              >
+              >{name}
               </h3>
               <div className='flex items-center gap-[6px]'>
                 <span className='flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px]
                 lg:leading-7 font-semibold text-headingColor'>
-                  <img src={starIcon}/>4.8 {/*=====DOCTOR IMAGE====*/}
+                  <img src={starIcon}/>{averageRating} {/*=====DOCTOR IMAGE====*/}
                 </span>
                 <span className='text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] 
-                text-textColor'>(272)</span>
+                text-textColor'>({totalRating})</span>
               </div>
 
               <p className=' md:text-[15px] max-w-[390px] 
-               text-[14px] leading-[30px] font-[400] text-textColor mt-[18px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima?
+               text-[14px] leading-[30px] font-[400] text-textColor mt-[18px]'>
+                {bio}
               </p>
           </div>
         </div>
@@ -70,21 +85,28 @@ const DoctorDetail = () => {
 
         <div className='mt-[50px]'>
           {
-            tab === 'about' && <DoctorAbout/>
+            tab === 'about' && <DoctorAbout name={name}
+            about={about} qualification={qualification}
+            experiences={experiences}/>
           },
           {
-            tab === 'feedback' && <Feedback/>
+            tab === 'feedback' && <Feedback reviews={reviews}
+            totalRating={totalRating}/>
           }
         </div>
 
       </div>
 
     <div>
-      <SidePanel/>
+      <SidePanel 
+      doctorId = {doctor._id} 
+      feePrice={feePrice}
+      timeSlots={timeSlots}
+      />
     </div>
 
       </div>
-    </div>
+    </div>)}
    </section>
   )
 }
